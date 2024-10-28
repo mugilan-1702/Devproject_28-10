b7037b1e91884fec6729f6e6f5a644c726d227ff
mugilan-1702 <152867791+mugilan-1702@users.noreply.github.com>
2024-10-28 18:27:27 +0530

    Update main.tf
    data block update

--- DevOps_Project_2/Terraform/main.tf
+++ DevOps_Project_2/Terraform/main.tf
@@ -3,19 +3,14 @@ data "aws_ami" "amazon-linux" {
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+    values = ["Jenkins_ami"]
   }
 }
 
 resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
-  key_name = "euran-jenkins"
+  key_name = "amzkey"
 
   tags = {
     Environment = "dev"
